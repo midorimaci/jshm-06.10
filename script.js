9b1f45d4f72fce0c228edaa7debf32871952aae6
midorimaci <55018122+midorimaci@users.noreply.github.com>
2019-10-09 11:45:34 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var product = prompt("Please, type day of the week");
+
+switch (product) {
+  case "Monday":
+    alert("Working Day");
+    break;
+  case "Tuesday":
+    alert("Working Day");
+    break;
+  case "Wednesday":
+    alert("Working Day");
+    break;
+  case "Thursday":
+    alert("Working Day");
+    break;
+  case "Friday":
+    alert("Working Day");
+    break;
+  case "Saturday":
+    alert("Weekend");
+    break;
+  case "Sunday":
+    alert("Weekend");
+    break;
+  default:
+    alert("Can not define, please try another value");
+    break;
+}
